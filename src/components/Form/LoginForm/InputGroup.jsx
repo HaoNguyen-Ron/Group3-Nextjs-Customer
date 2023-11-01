0a682a06c39ae207b6caa8e7b8657988e369bf70
Saltymeatball <hao100319@gmail.com>
2023-11-01 17:01:01 +0700

    fixing color theme

--- src/components/Form/LoginForm/InputGroup.jsx
+++ src/components/Form/LoginForm/InputGroup.jsx
@@ -1,14 +1,13 @@
 import { TextField } from '@mui/material';
+import { makeStyles, ThemeProvider } from '@mui/styles';
 import React, { memo, useMemo } from 'react';
 
 function InputGroup({
     label,
     type = 'text',
     name,
-    placeholder,
     validation,
 }) {
-
     const isValid = useMemo(() => {
         if (validation.errors[name] && validation.touched[name]) {
             return false;
@@ -18,26 +17,25 @@ function InputGroup({
     }, [name, validation.errors, validation.touched]);
 
     return (
-        <div className="mb-3">
+            <div className="mb-3">
+                <TextField fullWidth
+                    label={label}
+                    variant="outlined"
+                    type={type}
+                    name={name}
+                    value={validation.values[name]}
+                    onChange={validation.handleChange}
+                    onBlur={validation.handleBlur}
+                    className={` ${isValid ? '' : 'is-invalid'} ${classes.field}`}
+                    color='primary'
+                />
 
-            <TextField fullWidth
-                label={label}
-                variant="outlined"
-                type={type}
-                placeholder={placeholder}
-                name={name}
-                value={validation.values[name]}
-                onChange={validation.handleChange}
-                onBlur={validation.handleBlur}
-                className={` ${isValid ? '' : 'is-invalid'}` }
-            />
-
-            {!isValid && (
-                <div className="invalid-feedback">
-                    {validation.errors[name]}
-                </div>
-            )}
-        </div>
+                {!isValid && (
+                    <div className="invalid-feedback" style={{ color: '#ee2d7a' }}>
+                        {validation.errors[name]}
+                    </div>
+                )}
+            </div>
     );
 }
 
--- src/components/LoginPart/LoginPart.jsx
+++ src/components/LoginPart/LoginPart.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function LoginPart() {
@@ -6,17 +7,16 @@ export default function LoginPart() {
             <i className="fa-regular fa-user fs-4 my-auto me-2"></i>
             <div className=''>
                 <div>
-                    <span>
-                        Login
-                    </span>
                     <span >
                         Register
                     </span>
                 </div>
-                <div className='d-flex'>
-                    <h5 >My account</h5>
-                    <span><i className="fa-solid fa-chevron-down fs-4"></i></span>
-                </div>
+                <Link href='/login'>
+                    <div className='d-flex'>
+                        <h5 >Login</h5>
+                        <span><i className="fa-solid fa-chevron-down fs-4"></i></span>
+                    </div>
+                </Link>
 
             </div>
 
