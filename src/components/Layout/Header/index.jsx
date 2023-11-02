edb957f19771b1b3a6710dfa3ef263b2f9fa565c
HaoNguyen-Ron <hao100319@gmail.com>
2023-11-02 18:40:19 +0700

    update

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -15,15 +15,15 @@ export default function Header() {
             <header>
                 <div className="container-fluid">
                     <div className={`d-flex justify-content-center align-items-center align-self-center row styles.headerContainer`}>
-                        <div className='col-xs-12 col-sm-10 col-md-2 col-lg-2'>
+                        <div className='col-xs-12 col-sm-10 col-md-8 col-lg-2'>
                             <Logo />
                         </div>
 
-                        <div className={`col-xs-12 col-sm-1 col-md-1 col-lg-2 ${styles.media_tablet}`}>
+                        <div className={`col-xs-12 col-sm-1 col-md-2 col-lg-2 ${styles.media_tablet}`}>
                             <LoginPartTablet />
                         </div>
 
-                        <div className='col-xs-12 col-sm-12 col-md-7 col-lg-8 d-flex justify-content-center align-items-center'>
+                        <div className='col-xs-12 col-sm-12 col-md-12 col-lg-8 d-flex justify-content-center align-items-center'>
                             <SearchBar />
                         </div>
 
