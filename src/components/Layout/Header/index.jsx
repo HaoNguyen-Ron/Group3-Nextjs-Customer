6fa58777997edde1ca212ccbeba4de29a5980487
Saltymeatball <hao100319@gmail.com>
2023-11-02 11:21:33 +0700

    add reposive

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -15,11 +15,11 @@ export default function Header() {
                             <Logo />
                         </div>
 
-                        <div className='col-sm-12 col-md-10 col-lg-8 d-flex justify-content-center align-items-center'>
+                        <div className='col-sm-12 col-md-11 col-lg-8 d-flex justify-content-center align-items-center'>
                             <SearchBar />
                         </div>
 
-                        <div className='col-sm-12 col-md-2 col-lg-2'>
+                        <div className='col-sm-12 col-md-1 col-lg-2'>
                             <LoginPart />
                         </div>
                     </div>
--- src/components/LoginPart/LoginPart.jsx
+++ src/components/LoginPart/LoginPart.jsx
@@ -1,21 +1,32 @@
 import Link from 'next/link'
 import React from 'react'
+import styles from './loginPart.module.css'
 
 export default function LoginPart() {
     return (
-        <div className='d-flex'>
-            <i className="fa-regular fa-user fs-4 my-auto me-2"></i>
-            <div className='d-flex flex-column ms-2'>
-                <Link href='/register' style={{color: 'var(--main-color)'}}>
-                    <p> Register</p>
-                </Link>
+        <>
+            <div className={styles.media_pc}>
+                <div className='d-flex'>
+                    <i className="fa-regular fa-user fs-4 my-auto me-2"></i>
+                    <div className='d-flex flex-column ms-2'>
+                        <Link href='/register' style={{ color: 'var(--main-color)' }}>
+                            <p> Register</p>
+                        </Link>
 
-                <Link href='/login' style={{color: 'var(--main-color)'}}>
-                    <h5 >Login</h5>
-                </Link>
-
+                        <Link href='/login' style={{ color: 'var(--main-color)' }}>
+                            <h5 >Login</h5>
+                        </Link>
+                    </div>
+                </div>
             </div>
 
-        </div>
+            <div className={styles.media_tablet}>
+                <div className='d-flex' >
+                    <Link href='/login'>
+                        <i className="fa-regular fa-user fs-4 my-auto me-2" style={{ color: 'var(--main-color)' }}></i>
+                    </Link>
+                </div>
+            </div>
+        </>
     )
 }
