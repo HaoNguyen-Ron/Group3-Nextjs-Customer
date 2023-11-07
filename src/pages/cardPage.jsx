aa4cdd90d5d94264b8e9f2dc46881c11e9d55b90
HaoNguyen-Ron <hao100319@gmail.com>
2023-11-07 15:01:00 +0700

    fixing address input

--- src/pages/cardPage.jsx
+++ src/pages/cardPage.jsx
@@ -1,26 +1,12 @@
-<<<<<<< HEAD
-import CardList from '@/components/CardList';
-import React from 'react';
-
-function CardPage(props) {
-    return (
-        <div>
-            <CardList />
-=======
-import Cart from '@/components/Cart';
+import Card from '@/components/CardList/Card';
 import React from 'react';
 
 function CartPage(props) {
     return (
         <div>
-            <Cart />
->>>>>>> e3a996f20787821e3e9bf94147af924abcea1c16
+            <Card />
         </div>
     );
 }
 
-<<<<<<< HEAD
-export default CardPage;
-=======
 export default CartPage;
->>>>>>> e3a996f20787821e3e9bf94147af924abcea1c16
