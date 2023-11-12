b534a3e0498879a259afe6e2cf350af66dfb3723
hoanghung <hunggl160@gmail.com>
2023-11-12 20:41:18 +0700

    colle

---
+++ src/pages/collections/index.js
@@ -0,0 +1,141 @@
+import React from "react";
+import fil from "@/styles/Filter.module.css";
+
+function Filter(props) {
+  return (
+    <>
+      <div className={`${fil["collection-heading"]}`}>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-9 col-sm-12 col-xs-12">
+              <h1 className={`${fil["title"]}`}>Hàng có sẵn</h1>
+            </div>
+            <div className="col-md-3 col-sm-12 col-xs-12">
+              <div className={`${fil["collection-sortbyfilter-container"]}`}>
+                <div className={`${fil["collection-sortby-filter"]}`}>
+                  <div className={`${fil["collection-filterby"]}`}>
+                    <div
+                      className={`${fil["layered_filter_title"]} ${fil["boxstyle-mb"]}`}
+                    >
+                      <p className={`${fil["title_filter"]}`}>
+                        <span className={`${fil["icon-filter"]}`}>
+                          {/* <i className="fa-solid fa-caret-down" /> */}
+                        </span>
+                        <span className={`${fil["icon-close"]}`}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={24}
+                            height={24}
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+                          </svg>
+                        </span>
+                        Phân loại
+                      </p>
+                    </div>
+                  </div>
+                  <div className={`${fil["collection-sortby"]}`}>
+                    <div
+                      className={`${fil["layered_filter_title"]} ${fil["boxstyle-mb"]}`}
+                    >
+                      <p className={`${fil["title_filter"]}`}>
+                        <span className={`${fil["icon-filter"]}`}>
+                          <i
+                            className="fa fa-sort-alpha-asc"
+                            aria-hidden="true"
+                          />
+                        </span>
+                        <span className={`${fil["icon-close"]}`}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={24}
+                            height={24}
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+                          </svg>
+                        </span>
+                        Sắp xếp
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className="collection-sortby-option layered_filter_mobileContent"
+                  id="layered_sortby_mobile"
+                >
+                  <ul className="sort-by sort-by-content">
+                    <li className="">
+                      <span data-value="manual" data-filter="">
+                        Sản phẩm nổi bật
+                      </span>
+                    </li>
+                    <li className="active">
+                      <span
+                        data-value="price-ascending"
+                        data-filter="(price:product=asc)"
+                      >
+                        Giá: Tăng dần
+                      </span>
+                    </li>
+                    <li className="">
+                      <span
+                        data-value="price-descending"
+                        data-filter="(price:product=desc)"
+                      >
+                        Giá: Giảm dần
+                      </span>
+                    </li>
+                    <li className="">
+                      <span
+                        data-value="title-ascending"
+                        data-filter="(title:product=asc)"
+                      >
+                        Tên: A-Z
+                      </span>
+                    </li>
+                    <li>
+                      <span
+                        data-value="title-descending"
+                        data-filter="(title:product=desc)"
+                      >
+                        Tên: Z-A
+                      </span>
+                    </li>
+                    <li>
+                      <span
+                        data-value="created-ascending"
+                        data-filter="(updated_at:product=asc)"
+                      >
+                        Cũ nhất
+                      </span>
+                    </li>
+                    <li>
+                      <span
+                        data-value="created-descending"
+                        data-filter="(updated_at:product=desc)"
+                      >
+                        Mới nhất
+                      </span>
+                    </li>
+                    <li>
+                      <span
+                        data-value="best-selling"
+                        data-filter="(sold_quantity:product=desc)"
+                      >
+                        Bán chạy nhất
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Filter;
