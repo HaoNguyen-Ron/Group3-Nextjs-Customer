4212e6fdcf6ceef73c6fe9f34c85c6cdba8dc5ee
hoanghung <hunggl160@gmail.com>
2023-12-03 19:03:26 +0700

    abc

--- src/components/Layout/Header/Searchbar/SearchList.jsx
+++ src/components/Layout/Header/Searchbar/SearchList.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from "react";
 
-import styles from '@/styles/searchbar.module.css'
+import styles from "@/styles/searchbar.module.css";
 
 export default function SearchList({ searchList, index }) {
+  return (
+    <>
+      <li
+        className={`d-flex justify-content-between align-items-center ${styles["searchbar__input-li"]}`}
+        key={searchList._id}
+      >
+        <div>
+          <p>{searchList.name}</p>
+          <p className={styles["searchbar__input-p"]}>{searchList.price}</p>
+        </div>
 
-    return (
-        <>
-            <li className={`d-flex justify-content-between align-items-center ${styles['searchbar__input-li']}`} key={searchList._id}>
-                <div>
-                    <p>{searchList.name}</p>
-                    <p className={styles['searchbar__input-p']}>{searchList.price}</p>
-                </div>
-
-                <div>
-                    <img src={searchList.description} alt="" className={styles['searchbar__input-img']} />
-                </div>
-            </li>
-        </>
-
-    )
-};
+        <div>
+          <img
+            src={searchList.description}
+            alt=""
+            className={styles["searchbar__input-img"]}
+          />
+        </div>
+      </li>
+    </>
+  );
+}
