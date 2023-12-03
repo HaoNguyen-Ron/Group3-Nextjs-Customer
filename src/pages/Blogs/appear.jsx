2235ac3700bf7ec1c62c5352e53cde5a4eb7de89
hoanghung <hunggl160@gmail.com>
2023-12-03 17:50:51 +0700

    blog

--- src/pages/Blogs/appear.jsx
+++ src/pages/Blogs/appear.jsx
@@ -1,19 +1,46 @@
-
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import x from "@/pages/Blogs/instruct.module.css";
 import abo from "@/styles/AboutUs.module.css";
+import pay from "@/styles/Payment.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import Social from "@/components/social";
 
 function Appper(props) {
-
   const [isNavVisible, setNavVisible] = useState(false);
 
   const toggleNav = () => {
     setNavVisible(!isNavVisible);
   };
+  const [isCategoryOpen, setIsCategoryOpen] = useState(false);
+  const [isCategoryOpen2, setIsCategoryOpen2] = useState(false);
+  const [isMdScreen, setIsMdScreen] = useState(true);
 
-  
+  useEffect(() => {
+    // Sử dụng window.innerWidth để xác định kích thước màn hình
+    const handleResize = () => {
+      setIsMdScreen(window.innerWidth >= 768); // Đặt breakpoint 768px cho màn hình md
+    };
+
+    // Đăng ký sự kiện resize để theo dõi thay đổi kích thước màn hình
+    window.addEventListener("resize", handleResize);
+    // Kiểm tra kích thước màn hình khi component được tạo ra
+    handleResize();
+
+    // Hủy đăng ký sự kiện khi component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleCategory = () => {
+    setIsCategoryOpen(!isCategoryOpen);
+  };
+
+  const toggleCategory2 = () => {
+    setIsCategoryOpen2(!isCategoryOpen2);
+  };
+
   return (
     <div>
       <div className="container">
@@ -161,77 +188,211 @@ function Appper(props) {
             </div>
           </div>
           <div className="col-lg-3 col-sm-12 col-xs-12">
-              <aside className="sidebar-page">
-                <div
-                  className={`${abo["sidebox"]} mt-4 mobile-dropdown`}
-                  onClick={toggleNav}
-                >
-                  <div className={`${abo["about-head-ul"]} py-2`}>
-                    <h4
-                      className={`${abo["about-head-ul"]}} d-flex align-items-center`}
-                    >
-                      Danh mục
-                      <span
-                        className={`fa fa-angle-${
-                          isNavVisible ? "up" : "down"
-                        } d-block d-lg-none position-absolute`}
-                        style={{
-                          fontSize: "18px",
-                          right: "0",
-                        }}
-                      />
-                    </h4>
-                  </div>
-                  <div
-                    className={`d-lg-block ${isNavVisible ? "block" : "none"}`}
+            <aside className="sidebar-page">
+              <div
+                className={`${abo["sidebox"]} mt-4 mobile-dropdown`}
+                onClick={toggleNav}
+              >
+                <div className={`${abo["about-head-ul"]} py-2`}>
+                  <h4
+                    className={`${abo["about-head-ul"]}} d-flex align-items-center`}
                   >
-                    <ul
-                      className={`${abo["about-head-ul"]} ${
-                        isNavVisible ? "d-none" : "d-lg-block"
-                      }`}
-                    >
-                      <li className={`${abo["about-head-li"]} border-top py-2`}>
-                        <Link
-                          href="/"
-                          title="Tìm kiếm"
-                          className={`${abo["about-head-a"]}`}
-                        >
-                          <span>Tìm kiếm</span>
-                        </Link>
-                      </li>
-                      <li
-                        className={`${abo["about-head-li"]} nav-item border-top py-2`}
+                    Danh mục
+                    <span
+                      className={`fa fa-angle-${
+                        isNavVisible ? "up" : "down"
+                      } d-block d-lg-none position-absolute`}
+                      style={{
+                        fontSize: "18px",
+                        right: "0",
+                      }}
+                    />
+                  </h4>
+                </div>
+                <div
+                  className={`d-lg-block ${isNavVisible ? "block" : "none"}`}
+                >
+                  <ul
+                    className={`${abo["about-head-ul"]} ${
+                      isNavVisible ? "d-none" : "d-lg-block"
+                    }`}
+                  >
+                    <li className={`${abo["about-head-li"]} border-top py-2`}>
+                      <Link
+                        href="/"
+                        title="Tìm kiếm"
+                        className={`${abo["about-head-a"]}`}
                       >
-                        <Link
-                          className={`${abo["about-head-a"]}`}
-                          href="/about-us"
-                          title="Giới thiệu"
-                        >
-                          <span>Giới thiệu</span>
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
+                        <span>Tìm kiếm</span>
+                      </Link>
+                    </li>
+                    <li
+                      className={`${abo["about-head-li"]} nav-item border-top py-2`}
+                    >
+                      <Link
+                        className={`${abo["about-head-a"]}`}
+                        href="/about-us"
+                        title="Giới thiệu"
+                      >
+                        <span>Giới thiệu</span>
+                      </Link>
+                    </li>
+                  </ul>
                 </div>
-                <div className="group-sidebox-banner">
-                  <figure className="mb-0 mt-4">
-                    <a className={`${abo["about-head-a"]}`} href="#">
-                      <Image
-                        className="position-static"
-                        src="/assets/images/about_sibar.jpg"
-                        alt="about-side"
-                        layout="fill"
-                        objectFit="cover"
+              </div>
+              <div className="group-sidebox-banner">
+                <figure className="mb-0 mt-4">
+                  <a className={`${abo["about-head-a"]}`} href="#">
+                    <Image
+                      className="position-static"
+                      src="/assets/images/about_sibar.jpg"
+                      alt="about-side"
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </a>
+                </figure>
+              </div>
+            </aside>
+            <aside class={pay["side"]}>
+              {/* phần bài viết mới */}
+              <div
+                className={`${pay["group-sidebox"]} ${
+                  isMdScreen ? "active" : isCategoryOpen ? "active" : ""
+                }`}
+              >
+                <div
+                  className={`${pay["sidebox-title"]}`}
+                  onClick={toggleCategory}
+                >
+                  <h3 className={`${pay["htitle"]} d-flex align-items-center`}>
+                    Bài viết mới nhất
+                    <span
+                      className={`fa ${
+                        isCategoryOpen ? "fa-angle-up" : "fa-angle-down"
+                      } d-block d-md-none position-absolute`}
+                      style={{ right: "0" }}
+                    />
+                  </h3>
+                </div>
+
+                {/* phần content bài viết */}
+                <div
+                  className={`${pay["sidebox-content "]} ${
+                    isMdScreen || isCategoryOpen ? "d-md-block" : "d-none"
+                  }`}
+                >
+                  <ul
+                    className={`${pay["menuList-links"]} ${
+                      isMdScreen || isCategoryOpen ? "d-md-block" : "d-none"
+                    }`}
+                  >
+                    <BlogItem
+                      image="https://file.hstatic.net/1000160337/article/vi_sao_nen_dat_hang_som_tai_japanfigure.jpg"
+                      title="Figure Nhật Bản là những sản phẩm sản xuất HÀNG LOẠT là đúng!"
+                      title2="Figure Nhật Bản là những sản phẩm sản xuất HÀNG LOẠT là đúng!"
+                      title3="Tin Tức"
+                      date=" - 17/02/2023"
+                      href="/Blogs/appear"
+                    />
+
+                    <BlogItem
+                      image="/assets/images/blog_2.jpg"
+                      title="Cách Tôi Kiếm Được 250k Mỗi Giờ"
+                      title2="Cách Tôi Kiếm Được 250k Mỗi Giờ"
+                      title3="Tin Tức"
+                      date=" - 12/02/2022"
+                      href="/Blogs/instruct"
+                    />
+                    <BlogItem
+                      image="/assets/images/blog_3.jpg"
+                      title="Kakeibo phương pháp tiết kiệm, chi tiêu hợp lý"
+                      title2="Kakeibo phương pháp tiết kiệm, chi tiêu hợp lý"
+                      title3="Hưỡng Dẫn"
+                      date=" - 23/04/2022"
+                      href="/Blogs/Kakeibo"
+                    />
+                    <BlogItem
+                      image="/assets/images/blog_4.jpg"
+                      title="Hỏi Và Đáp"
+                      title2="Hỏi Và Đáp"
+                      title3="Tin Tức"
+                      date=" - 23/04/2022"
+                      href="/answer"
+                    />
+                  </ul>
+                </div>
+              </div>
+
+              {/* phần danh mục */}
+              {/* <div className={`${pay["group-sidebox"]}`}>
+                  <div
+                    className={`${pay["sidebox-title"]}`}
+                    onClick={toggleCategory2}
+                  >
+                    <h3
+                      className={`${pay["htitle"]} d-flex align-items-center`}
+                    >
+                      Danh Mục
+                      <span
+                        className={`fa ${
+                          isCategoryOpen2 ? "fa-angle-up" : "fa-angle-down"
+                        } d-block d-md-none position-absolute`}
+                        style={{ right: "0" }}
                       />
-                    </a>
-                  </figure>
-                </div>
-              </aside>
-            </div>
+                    </h3>
+                  </div>
+
+            
+                  <div
+                    className={`${pay["sidebox-content "]} ${
+                      isMdScreen || isCategoryOpen2 ? "d-md-block" : "d-none"
+                    }`}
+                  >
+                  
+                    
+                  </div>
+                </div> */}
+            </aside>
+          </div>
         </div>
       </div>
+      <Social />
     </div>
   );
 }
 
 export default Appper;
+const BlogItem = ({ image, title, title2, title3, title4, date, href }) => {
+  return (
+    <>
+      <li>
+        <div className={`${pay["item-article"]} d-flex clearfix`}>
+          <div className="post-image">
+            <a href={href}>
+              <img className={`${pay["lazyloaded"]}`} src={image} alt={title} />
+            </a>
+          </div>
+          <div className={`${pay["post-content"]} ms-2`}>
+            <h3 className={`${pay["fs14"]} mb-0`}>
+              <a href={href} className={pay["fs14_hover"]}>
+                {title2}
+              </a>
+            </h3>
+            <p className="mb-0">
+              <span>
+                <a href={href} className={pay["tag"]}>
+                  {title3}
+                </a>
+              </span>
+              <span>
+                <a href={href}>{title4}</a>
+              </span>
+              <span className="date">{date}</span>
+            </p>
+          </div>
+        </div>
+      </li>
+    </>
+  );
+};
