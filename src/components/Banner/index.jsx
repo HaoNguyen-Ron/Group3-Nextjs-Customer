7663df2bd6969920108d7ba316a231f1ab1eaf8f
Saltymeatball <hao100319@gmail.com>
2024-03-11 11:10:34 +0700

    fix footer

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import x from "@/components/Banner/Banner.module.css";
 import Link from "next/link";
+
 function Banner({ image, titles, caption, name }) {
   return (
     <div>
@@ -10,13 +11,18 @@ function Banner({ image, titles, caption, name }) {
             ${x["snip1278"]}`}
       >
         <h4 className={`${x["H4"]}`}>{titles}</h4>
+
         <div className={`${x["image"]}`}>
           <img className={`${x["images"]}`} src={image} alt="sq-sample6" />
         </div>
+
         <figcaption className={`${x["figcaption"]}`}>
           <p className="text-dark">Bộ sưu tập</p>
+
           <p className={`${x["name"]}`}>{name}</p>
+
           <p className={` text-dark ${x["pi"]}`}>{caption}</p>
+
           <div className={`${x["price"]}`}>
             <Link href="/collections" className={`${x["add-to-cart"]}`}>
               Xem Thêm
