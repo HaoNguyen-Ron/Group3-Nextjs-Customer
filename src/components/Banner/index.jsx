a8d9f02c40b9917d79231f986cecbae862afcefe
HaoNguyen-Ron <hao100319@gmail.com>
2024-03-26 14:02:51 +0700

    modify code

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import x from '@/components/Banner/Banner.module.css'
+import styles from '@/components/Banner/Banner.module.css'
 import Link from 'next/link'
 
 function Banner({ image, titles, caption, name }) {
@@ -7,24 +7,24 @@ function Banner({ image, titles, caption, name }) {
     <div>
       <figure
         className={`
-            ${x['figure']}
-            ${x['snip1278']}`}
+            ${styles['figure']}
+            ${styles['snip1278']}`}
       >
-        <h4 className={`${x['H4']}`}>{titles}</h4>
+        <h4 className={`${styles['H4']}`}>{titles}</h4>
 
-        <div className={`${x['image']}`}>
-          <img className={`${x['images']}`} src={image} alt='sq-sample6' />
+        <div className={`${styles['image']}`}>
+          <img className={`${styles['images']}`} src={image} alt='sq-sample6' />
         </div>
 
-        <figcaption className={`${x['figcaption']}`}>
+        <figcaption className={`${styles['figcaption']}`}>
           <p className='text-dark'>Bộ sưu tập</p>
 
-          <p className={`${x['name']}`}>{name}</p>
+          <p className={`${styles['name']}`}>{name}</p>
 
-          <p className={` text-dark ${x['pi']}`}>{caption}</p>
+          <p className={` text-dark ${styles['pi']}`}>{caption}</p>
 
-          <div className={`${x['price']}`}>
-            <Link href='/collections' className={`${x['add-to-cart']}`}>
+          <div className={`${styles['price']}`}>
+            <Link href='/collections' className={`${styles['add-to-cart']}`}>
               Xem Thêm
               <i className='fa fa-angle-right' aria-hidden='true'></i>
             </Link>
